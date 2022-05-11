44ed50998ea4e3fb86ee44190568832de4f447da
Bogdan <bodyaua1984@gmail.com>
2022-05-11 23:01:03 +0300

    end porject-1

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,7 +4,9 @@ export function Movies (props) {
     const {movies} = props
     return(
         <div className="movies">
-            {movies.map(movie => <Movie key={movie.imdbID} {...movie}/>)}
+            {movies?.length ? movies.map(movie => <Movie key={movie.imdbID} {...movie}/>) :
+            <h4>Nothing found!</h4>
+            }
         </div>
     )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,81 @@
 import React from "react";
-export class Search extends React.Component{
-    state = {
-        search: ''
+export class Search extends React.Component {
+  state = {
+    search: "",
+    type: "all",
+  };
+
+  handleKey = (event) => {
+    if (event.key === "Enter") {
+      this.props.searchMovies(this.state.search, this.state.type);
     }
-render(){
-    const {search} = this.state
+  };
+
+  handleFilter = (event) => {
+    this.setState(
+      () => ({ type: event.target.dataset.type }),
+      () => {
+        this.props.searchMovies(this.state.type, this.state.type);
+      }
+    );
+  };
+
+  render() {
+    const { search } = this.state;
     return (
-        
-        <div className="col s12">
-          <div className="input-field">
-            <input 
-            id="email_inline" 
-            type="search" 
-            className="validate" 
+      <div className="col s12">
+        <div className="input-field">
+          <input
+            type="search"
+            className="validate"
             placeholder="search"
             value={search}
-            onChange={(e) => this.setState({search: e.target.value})}
-            />
-          </div>
+            onChange={(e) => this.setState({ search: e.target.value })}
+            onKeyDown={this.handleKey}
+          />
+          <button
+            className="btn"
+            onClick={() =>
+              this.props.searchMovies(this.state.search, this.state.type)
+            }
+          >
+            Serach
+          </button>
+        </div>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="all"
+            onChange={this.handleFilter}
+            checked={this.state.type === "all"}
+          />
+          <span>All</span>
+        </label>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="movie"
+            onChange={this.handleFilter}
+            checked={this.state.type === "movie"}
+          />
+          <span>Movies only</span>
+        </label>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="series"
+            onChange={this.handleFilter}
+            checked={this.state.type === "series"}
+          />
+          <span>Series only</span>
+        </label>
       </div>
-    )
-}
+    );
+  }
 }
